c6d8a909ce8c85ec5e2e5790814dd9a2cafee0f2
Mariusz Krzysztof Przybysz <mariusz.krzysztof.przybysz@gmail.com>
2020-01-15 13:20:36 +0100

    Simulate a data server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,26 +2,28 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private inMemoryDataService: InMemoryDataService) { }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add("HeroService: fetched heroes");
-    return of(HEROES);
+    return of(this.inMemoryDataService.createDb().heroes);
   }
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     //Note the backticks ( ` ) that define a JavaScript template literal for embedding the id.
-    return of(HEROES.find(hero => hero.id === id));
+    return of(this.inMemoryDataService.createDb().heroes.find(hero => hero.id === id));
   }
 }
